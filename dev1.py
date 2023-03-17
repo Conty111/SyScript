0ed5f180cbeee79d7febbb6a758289e907b9f712
Pipi <mitj88@gmail.com>
2023-03-17 22:28:38 +0300

    PEP8 formatting

--- dev1.py
+++ dev1.py
@@ -1,9 +1,9 @@
-import csv
-import socket
 import datetime
+import socket
+import csv
+
 from pythonping import ping
 from socket import AF_INET, SOCK_STREAM
-
 
 
 sock = socket.socket(AF_INET, SOCK_STREAM)
@@ -12,9 +12,10 @@ TIMEOUT_PING = 4
 TIMEOUT_SOCKET = 4
 COUNT_OF_PACKET = 4
 
+
 class Checked_addresses():
-    def __init__(self, serv: list) -> None:
-        self.domain = serv[0]
+    def __init__(self, host_row: list):
+        self.domain = host_row[0]
         self.date = []
         self.ip_address = []
         self.packet_loss = []
@@ -22,27 +23,33 @@ class Checked_addresses():
         self.port = []
         self.port_status = []
 
-        for i in serv[1]:
-            # ICMP 
-            if serv[2] == []:
-                self.date.append(datetime.datetime.now().isoformat().replace("T", " "))
-                self.rtt.append(ping(i, count=COUNT_OF_PACKET, timeout=TIMEOUT_PING).rtt_avg_ms)
-                self.ip_address.append(i)
-                self.packet_loss.append(ping(i, count=COUNT_OF_PACKET, timeout=TIMEOUT_PING).packet_loss)
-                self.port.append(-1)
-                self.port_status.append("Ports are missing")
-                continue
+        for address in host_row[1]:
+            self.set_data(host_row[2], address)
 
-            # Other
-            for j in serv[2]:
-                res = check_adr(i, int(j))
+    def set_data(self, ports: list, address: str):
+        # ICMP ping если портов нет
+        if ports == []:
+            ping_adr = ping(address, count=COUNT_OF_PACKET, timeout=TIMEOUT_PING)
+            date = datetime.datetime.now().isoformat().replace("T", " ")
+            self.date.append(date)
+            self.rtt.append(ping_adr.rtt_avg_ms)
+            self.ip_address.append(address)
+            self.packet_loss.append(ping_adr.packet_loss)
+            self.port.append(-1)
+            self.port_status.append("Ports are missing")
+            return
 
-                self.date.append(datetime.datetime.now().isoformat().replace("T", " "))
-                self.rtt.append(res[0])
-                self.ip_address.append(i)
-                self.packet_loss.append(res[1])
-                self.port.append(j)
-                self.port_status.append(res[2])
+        # Если указаны порты
+        for port in ports:
+            characteristics = get_socket_characteristics(address, int(port))
+            date = datetime.datetime.now().isoformat().replace("T", " ")
+
+            self.date.append(date)
+            self.rtt.append(characteristics[0])
+            self.ip_address.append(address)
+            self.packet_loss.append(characteristics[1])
+            self.port.append(address)
+            self.port_status.append(characteristics[2])
 
     def show(self):
         for i in range(len(self.ip_address)):
@@ -53,40 +60,45 @@ class Checked_addresses():
             result += ' | ' + str(self.rtt[i]) + ' ms'
             result += ' | ' + str(self.port[i])
             result += ' | ' + str(self.port_status[i])
-            print(f"{result}")
-        print()
+            print(result)
+        print(f"\n")
+
 
 def make_adr_from_row(row: list) -> list:
-    # Преобразование в формат ['доменное имя', ['IP-адреса'], ['порты']] или ['???', ['IP-адрес'], False]
+    # Преобразование в формат ['доменное имя', ['IP-адреса'], ['порты']]
     if "." in row[0] or row[0] == 'localhost':
+        # Проверка: дано доменное имя или ip-адерс
         if not (row[0].replace(".", "", len(row[0]))).isdigit():
             row.insert(1, get_domain_adr(row[0]))
         else:
             try:
+                # Пытаемся добавить имя хоста/доменное имя, если оно есть
                 row.insert(0, socket.gethostbyaddr(row[0]))
-                row[1] = [row[1]]
             except:
                 row.insert(0, "Address hasn't hostname")
-                row[1] = [row[1]]
+            row[1] = [row[1]]
+
+        # Проверка на корректность введенных портов
         if (row[2].replace(",", "", len(row[2]))).isdigit():
             row[2] = row[2].split(",")
         else:
             row[2] = []
-    else:
-        return
 
-def check_adr(address: str, port: int) -> tuple:
+        return row
+    else:
+        return []
+
+
+def get_socket_characteristics(address: str, port: int) -> tuple:
+    ping_adr = ping(address, timeout=TIMEOUT_PING, count=COUNT_OF_PACKET)
+    rtt = ping_adr.rtt_avg_ms
+    packet_loss = ping_adr.packet_loss
     sock = socket.socket(AF_INET, SOCK_STREAM)
     sock.settimeout(TIMEOUT_SOCKET)
-    a = ping(address, timeout=TIMEOUT_PING, count=COUNT_OF_PACKET)
-    rtt = a.rtt_avg_ms
-    packet_loss = a.packet_loss
-    
     try:
         sock.connect((address, port))
-        return(rtt, float(packet_loss), "Opened")
+        return (rtt, float(packet_loss), "Opened")
     except:
-
         return (rtt, float(packet_loss), "Not opened")
 
 
@@ -97,11 +109,11 @@ def get_domain_adr(adr: str) -> str:
         return "Error in domain adress"
 
 
-
 with open("test.csv", newline='') as csvfile:
     file_input = csv.reader(csvfile, delimiter=";")
     for row in file_input:
         if row:
             row = make_adr_from_row(row)
-            row_of_addresses = Checked_addresses(row)
-            row_of_addresses.show()
+            if row != []:
+                row_of_addresses = Checked_addresses(row)
+                row_of_addresses.show()
